fe7d872f540a021ae4d738fadcc89d5c118cb871
Ann Sabie <annsabie1@gmail.com>
2021-09-29 16:10:23 -0500

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
 $(".saveBtn").on("click", function() {
     console.log(this)
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(text,time);
+})
+
+
+function highlightHour() {
+
+    var currentHour = moment().hour();
+    console.log(currentHour);
+
+    $(".time-block").each(function(){
+        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (hourBlock < currentHour) {
+        $(".description").addClass("past");
+        }
+
+        else if (hourBlock === currentHour) {
+        $(".description").addClass("present");
+        }
+
+        else {
+        $(".description").addClass("future");
+        }
+    })
 }
+
+highlightHour();
+
+
+
