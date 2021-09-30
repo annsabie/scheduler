cc4d63f5f60cb613d9744b900aa8c9617f079f4d
Ann Sabie <annsabie1@gmail.com>
2021-09-29 22:26:57 -0500

    final js

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,9 @@ $(".saveBtn").on("click", function() {
     console.log(this)
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-    localStorage.setItem(text,time);
+    localStorage.setItem(text, JSON.stringify(text));
+    
 })
-
 
 function highlightHour() {
 
@@ -12,23 +12,37 @@ function highlightHour() {
     console.log(currentHour);
 
     $(".time-block").each(function(){
-        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+        var schedHour = parseInt($(this).attr("id"));
+        console.log(schedHour,currentHour);
 
-        if (hourBlock < currentHour) {
+        if (schedHour < currentHour) {
         $(".description").addClass("past");
         }
 
-        else if (hourBlock === currentHour) {
+        if (schedHour == currentHour) {
         $(".description").addClass("present");
         }
 
         else {
-        $(".description").addClass("future");
+        $(".description").addClass("future")
         }
     })
 }
 
+$("#9 .description").val(localStorage.getItem("#9"));
+$("#10 .description").val(localStorage.getItem("#10"));
+$("#11 .description").val(localStorage.getItem("#11"));
+$("#12 .description").val(localStorage.getItem("#12"));
+$("#13 .description").val(localStorage.getItem("#13"));
+$("#14 .description").val(localStorage.getItem("#14"));
+$("#15 .description").val(localStorage.getItem("#15"));
+$("#16 .description").val(localStorage.getItem("#16"));
+$("#17 .description").val(localStorage.getItem("#17"));
+
 highlightHour();
+
+
+
 
 
 
